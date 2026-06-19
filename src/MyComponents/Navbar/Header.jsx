1a7a9e929a30b5f7ae30d18cb6ba72276a6ad2c2
Waqas Hakim <162884901+Waqashakim1122@users.noreply.github.com>
2026-06-20 01:38:04 +0500

    Refactor Header with dynamic nav items and scroll effect
    Refactor Header component to use navItems array and add scroll effect.

--- src/MyComponents/Navbar/Header.jsx
+++ src/MyComponents/Navbar/Header.jsx
@@ -1,60 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import logo from '../../assetes/navbar-logo.png';
+import './Header.css'
+
+const navItems = [
+  { to: '/', label: 'Home' },
+  { to: '/About', label: 'About' },
+  { to: '/courses', label: 'Courses' },
+  { to: '/ContactUs', label: 'Contact' },
+];
 
 const Header = () => {
-    return (
-        <nav className="navbar navbar-expand-md text-bg-light p-1 sticky-top">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">
-                    <img className="img-fluid" src={logo} alt="Learn Quran Online Logo" width="150" height="80"/>
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" aria-current="page" to="/">
-                                Home
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/About">
-                                About
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/courses">
-                                Courses
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/ContactUs">
-                                Contact
-                            </NavLink>
-                        </li>
-                        <li className="nav-item d-md-none">
-                            <NavLink className="btn btn-dark w-100 mt-2" to="/courses" role="button">
-                                Free Trial
-                            </NavLink>
-                        </li>
-                    </ul>
-                    <NavLink className="btn btn-dark d-none d-md-inline-block" to="/courses" role="button">
-                        Free Trial
-                    </NavLink>
-                </div>
-            </div>
-        </nav>
-    );
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => setScrolled(window.scrollY > 10);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return (
+    <header className={`site-header ${scrolled ? 'is-scrolled' : ''}`}>
+      <nav className="navbar navbar-expand-md p-0">
+        <div className="container-fluid site-header-inner">
+          <a className="navbar-brand site-logo" href="/">
+            <img src={logo} alt="Learn Quran Online Logo" width="140" height="auto" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto site-nav">
+              {navItems.map((item) => (
+                <li className="nav-item" key={item.to}>
+                  <NavLink className="nav-link" to={item.to} end>
+                    {item.label}
+                  </NavLink>
+                </li>
+              ))}
+              <li className="nav-item d-md-none mt-2">
+                <NavLink className="btn site-cta-btn w-100" to="/courses" role="button">
+                  Book Free Trial
+                </NavLink>
+              </li>
+            </ul>
+            <NavLink className="btn site-cta-btn d-none d-md-inline-block" to="/courses" role="button">
+              Book Free Trial
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
