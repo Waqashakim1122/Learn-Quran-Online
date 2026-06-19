8743f0b1b8b92e159d1cb48a60191cf0a4fd5022
Waqas Hakim <162884901+Waqashakim1122@users.noreply.github.com>
2026-06-20 01:36:36 +0500

    Remove font preconnect and stylesheet links
    Removed font link tags from index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Fraunces:wght@400;600;700&family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
