eba84aa10a1a426c12244b7706e2be10a1a3c43c
Waqas Hakim <162884901+Waqashakim1122@users.noreply.github.com>
2026-06-20 00:16:47 +0500

    Refactor Header component for better accessibility
    Updated logo alt text and adjusted button styles.

--- src/MyComponents/Navbar/Header.jsx
+++ src/MyComponents/Navbar/Header.jsx
@@ -3,13 +3,11 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../assetes/navbar-logo.png';
 
 const Header = () => {
-    
-
     return (
         <nav className="navbar navbar-expand-md text-bg-light p-1 sticky-top">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">
-                    <img className="img-fluid" src={logo} alt="Logo" width="150" height="80"/>
+                    <img className="img-fluid" src={logo} alt="Learn Quran Online Logo" width="150" height="80"/>
                 </a>
                 <button
                     className="navbar-toggler"
@@ -25,28 +23,27 @@ const Header = () => {
                 <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
                     <ul className="navbar-nav mx-auto">
                         <li className="nav-item">
-                            <NavLink className="nav-link active" aria-current="page" to="/">
+                            <NavLink className="nav-link" aria-current="page" to="/">
                                 Home
                             </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link active" aria-current="page" to="/About">
+                            <NavLink className="nav-link" to="/About">
                                 About
                             </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link active" aria-current="page" to="/courses">
-                                Courses 
+                            <NavLink className="nav-link" to="/courses">
+                                Courses
                             </NavLink>
                         </li>
-                       
                         <li className="nav-item">
-                            <NavLink className="nav-link active" aria-current="page" to="/ContactUs">
+                            <NavLink className="nav-link" to="/ContactUs">
                                 Contact
                             </NavLink>
                         </li>
                         <li className="nav-item d-md-none">
-                            <NavLink className="btn btn-dark w-100" to="/enroll" role="button">
+                            <NavLink className="btn btn-dark w-100 mt-2" to="/courses" role="button">
                                 Free Trial
                             </NavLink>
                         </li>
